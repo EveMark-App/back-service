28686234bccb0ca7c8edf2b80c923260e5aa93dc
Sami Kammoun <kammounsami77@gmail.com>
2023-01-10 21:53:33 +0100

    linting

--- app.js
+++ app.js
@@ -1,40 +1,36 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require("cloudinary").v2;
 dotenv.config();
 
-var indexRouter = require('./routes/index');
-var uploadRouter = require('./routes/upload');
+var indexRouter = require("./routes/index");
+var uploadRouter = require("./routes/upload");
 
-var usersRouter = require('./routes/user');
-const eventsRouter = require('./routes/event');
+var usersRouter = require("./routes/user");
+const eventsRouter = require("./routes/event");
 
 var app = express();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.use(
-    cors()
-  );
+app.use(cors());
 
-
-
-app.use(logger('dev'));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(logger("dev"));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 const mongo_uri = "mongodb+srv://evemark:evemark1234@cluster0.g3yelbg.mongodb.net/?retryWrites=true&w=majority";
 mongoose.set("strictQuery", false);
@@ -46,10 +42,10 @@ mongoose.connect(mongo_uri, function (err) {
   }
 });
 console.log(cloudinary.config());
-app.use('/', indexRouter);
-app.use('/upload', uploadRouter);
+app.use("/", indexRouter);
+app.use("/upload", uploadRouter);
 
-app.use('/user', usersRouter);
-app.use('/event', eventsRouter);
+app.use("/user", usersRouter);
+app.use("/event", eventsRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var QRCode = require('qrcode')
-const cloudinary = require('cloudinary').v2;
-const isAuthenticated = require('../middleware/isAuthenticated');
+var QRCode = require("qrcode");
+const cloudinary = require("cloudinary").v2;
+const isAuthenticated = require("../middleware/isAuthenticated");
 
-const fs = require('fs');
+const fs = require("fs");
 
 /* GET home page. */
-router.get('/',isAuthenticated, async function(req, res, next) {
-
-
-  res.status(200).json({msg:"good"})
-//   try {
-//    // const fileStr = fs.readFileSync(__dirname+'/a.png', {encoding: 'base64'});
-//     const uploadResponse = await cloudinary.uploader.upload(__dirname+'/a.png');
-//       console.log(uploadResponse)
-//     res.json(uploadResponse);
-// } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ err: 'Something went wrong' });
-// }
+router.get("/", async function (req, res, next) {
+  res.status(200).json({ msg: "good" });
+  //   try {
+  //    // const fileStr = fs.readFileSync(__dirname+'/a.png', {encoding: 'base64'});
+  //     const uploadResponse = await cloudinary.uploader.upload(__dirname+'/a.png');
+  //       console.log(uploadResponse)
+  //     res.json(uploadResponse);
+  // } catch (err) {
+  //     console.error(err);
+  //     res.status(500).json({ err: 'Something went wrong' });
+  // }
   // QRCode.toDataURL('I am a pony!', function (err, url) {
   //   console.log(url)
-    
-//  })
+
+  //  })
 
   // QRCode.toString("",{type:'terminal'}, function (err, url) {
   //   console.log(url)
   // })
-  
 });
 
 module.exports = router;
