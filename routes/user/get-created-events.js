86ae4cc25d5cd16064514fbc7f97994aeaad6981
ZiedChekir <ziedchekir1@gmail.com>
2023-01-13 00:20:49 +0100

    Update get-created-events.js

--- routes/user/get-created-events.js
+++ routes/user/get-created-events.js
@@ -2,14 +2,15 @@ const User = require("../../models/User");
 
 module.exports = async function (req, res) {
   console.log("retrieving created events for user ");
-
+  console.log(req.user)
 try{
     const user =  await User.findOne({email:req.user.email}).populate("created_events")
+    console.log(user)
     res.status(200).json(user.created_events);
 
 }catch(e){
  
-    console.log("error while finding user");
-    res.status(500).send("Error retrieving events" + e);
+    console.log("error while finding user" + e);
+    res.status(500).json(e);
   }
 };
