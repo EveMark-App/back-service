486659211a7e3242d971f307f1a8acdbae5f4efc
ZiedChekir <ziedchekir1@gmail.com>
2023-01-13 19:16:22 +0100

    fix checkin

--- routes/event/checkin.js
+++ routes/event/checkin.js
@@ -4,11 +4,30 @@ const User = require("../../models/User");
 module.exports = async function (req, res) {
   console.log("buying ticket for event ", req.body.eventId);
 
+  //check that the request comes from the creator
+  //check that the participant is not yet checked in
+  //check that the participant is in the attendees list of the event
+  //check in successfull
   try {
-    await Event.findByIdAndUpdate(req.body.eventId, {
-      $push: { checked_in_attendees: req.user.id },
+    const creator = await User.findById(req.user.id);
+    const event = await Event.findById(req.body.eventId);
+    const participantId = req.body.participantId;
+    if (creator._id != event.creator)
+      res.status(400).json({ error: "You are not the creator of the event" });
+    if (!event.attendees.includes(participantId))
+      res
+        .status(400)
+        .json({ error: "the participant is not in the attendees list" });
+    if (event.checked_in_attendees.includes(participantId))
+      res
+        .status(400)
+        .json({ error: "the participant is already checked in" });
+
+    await Event.updateOne({_id:event._id}, {
+      $push: { checked_in_attendees: participantId },
     });
-    const user = await User.findOne({_id:req.user.id});
+    
+    const user = await User.findOne({ _id: participantId });
     res.status(200).json(user);
   } catch (e) {
     console.log(e);
