09d91cac19877cb121bbfaa490e38939ed93870e
ZiedChekir <ziedchekir1@gmail.com>
2023-01-13 17:44:40 +0100

    buy api

--- routes/event/buy.js
+++ routes/event/buy.js
@@ -4,6 +4,10 @@ const User = require("../../models/User");
 module.exports = async function (req, res) {
   console.log("buying ticket for event ", req.body.eventId);
   try {
+    const event = Event.findById(req.body.eventId)
+    if(event.attendees.includes(req.user.id)){
+      return res.status(500).json({error:"You are already registred"});
+    }
     await Event.findByIdAndUpdate(req.body.eventId, {
       $push: { attendees: req.user.id },
     });
@@ -15,6 +19,6 @@ module.exports = async function (req, res) {
     res.status(200).json({ msg: "Payment Sucessfull" });
   } catch (e) {
     console.log(e);
-    res.status(500).json({ error: "Payment failed" + e });
+    res.status(500).json({ error: "Payment failed" });
   }
 };
