75aa782b63629bbf55155141629ce9706189f5dc
ZiedChekir <ziedchekir1@gmail.com>
2023-01-13 18:20:37 +0100

    mail router

--- routes/mail.js
+++ routes/mail.js
@@ -3,14 +3,11 @@ var express = require('express');
 var router = express.Router();
 
 
-router.post('/', async (req, res) => {
-    try {
-        req.body
-        res.status(200).json(uploadResponse);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ err: 'Something went wrong' });
-    }
+router.post('/', isAuthenticated, (req, res) => {
+    
+
+        res.status(200).json({msg:"hello"});
+  
 });
 
 module.exports = router;
