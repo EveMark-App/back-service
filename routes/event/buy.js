358486cbe805c351f65b5cc48cb4f4d40414fe59
ZiedChekir <ziedchekir1@gmail.com>
2023-01-13 20:51:35 +0100

    buy transaction

--- routes/event/buy.js
+++ routes/event/buy.js
@@ -1,24 +1,35 @@
 const Event = require("../../models/Event");
 const User = require("../../models/User");
+const mongoose = require("mongoose");
 
 module.exports = async function (req, res) {
   console.log("buying ticket for event ", req.body.eventId);
+  const conn = mongoose.connection;
+  const session = await conn.startSession();
   try {
-    const event = await Event.findById(req.body.eventId)
-    if(event.attendees.includes(req.user.id)){
-      return res.status(500).json({error:"You are already registred"});
+    session.startTransaction();
+
+    const event = await Event.findById(req.body.eventId);
+
+    if (event.attendees.includes(req.user.id)) {
+      return res.status(500).json({ error: "You are already registred in this event" });
     }
-    await Event.findByIdAndUpdate(req.body.eventId, {
+
+    await Event.updateOne({_id:event._id}, {
       $push: { attendees: req.user.id },
     });
 
-    await User.findByIdAndUpdate(req.user.id, {
+    await User.updateOne({_id:req.user.id}, {
       $push: { my_events: req.body.eventId },
     });
 
+    await session.commitTransaction();
     res.status(200).json({ msg: "Payment Sucessfull" });
-  } catch (e) {
-    console.log(e);
+    console.log("success");
+  } catch (error) {
+    console.log("error");
+    await session.abortTransaction();
     res.status(500).json({ error: "Payment failed" });
+    session.endSession();
   }
 };
